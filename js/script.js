7723295b33f26d3f04b9cee53130da037d1112ef
Emma Guy <dusk@Vampiredusk.local>
2019-05-07 22:27:50 +1200

    made start on task

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var shoppingList= ["Pears", "Milk", "Bread", "Egg", "Cheese"]
+console.log(shoppingList);
+
+
+
+function namePrompt(){
+  console.log("this is name prompt");
+  var name= prompt ("What's on your shopping List?");
+  //console.log("Hello" + name);
+  document.getElementById('shoppingItems');
+}
